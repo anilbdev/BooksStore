1fe8db0c2a92cb3547233f412dc9e3ae41f88d40
bhasi <anilbhasib@gmail.com>
2021-12-09 11:11:50 +0530

    vector icon linked

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 const HomeScreen = () => {
     return (
         <View>
             <Text>Home Screen</Text>
+            <Icon name="shopping-cart" size={30} color="#900" />
         </View>
     )
 }

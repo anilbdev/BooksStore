f20664945bd55c4305254d2d3afb5440faa69090
bhasi <anilbhasib@gmail.com>
2021-12-10 03:19:08 +0530

    length of cart array needs to be printed

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,27 +1,79 @@
 import React from 'react'
-import { useSelector,useDispatch } from 'react-redux';
-import { StyleSheet, Text, View,Button } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux';
+import { StyleSheet, Text, View, FlatList, Image,TouchableOpacity } from 'react-native'
 //custom import
 import {
     increment,
     decrement,
-    ListItem
+    ListItem,
+    removeFromCart,
+    Button
 } from '..'
 
-
+const renderCartList = (book,dispatch) => {
+   
+    return (
+        <View style={styles.cartListContainer}>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={{ uri: 'https://media.istockphoto.com/photos/vintage-album-cover-texture-frame-for-background-picture-id1297066744' }} />
+            </View>
+            <View style={styles.detailsContainer}>
+                <View style={styles.bookDetails}>
+                    <Text style={{ fontSize: 28 }}>{book.item.title}</Text>
+                    <Text style={{ fontSize: 20 }}>{book.item.auther}</Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                   
+                </View>
+            </View>
+        </View>
+    )
+}
 const CartScreen = () => {
-    const cart =useSelector((state) =>{
-      
-        return state.cartReducer})
-        console.log('cart SCREEN>>>>>>>>',cart)
+    const cart = useSelector((state) => {
+        return state.cartReducer
+    })
+  
     const dispatch = useDispatch()
     return (
-        <View>
-            <ListItem/>
+        <View style={styles.container}>
+            <View style={styles.listItemContainer}>
+                <FlatList
+                    data={cart}
+                    keyExtractor={cart => cart.title}
+                    renderItem={(book)=>renderCartList(book,dispatch)}
+                />
+            </View>
+
         </View>
     )
 }
 
 export default CartScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        borderWidth: 2
+    },
+    listItemContainer: {
+        padding: 15
+    },
+    cartListContainer: {
+        flexDirection: 'row',
+        marginVertical: 15
+    },
+    imageContainer: {},
+    image: {
+        height: 150,
+        width: 85
+    },
+    detailsContainer: {
+        justifyContent: 'space-between'
+    },
+    bookDetails: {
+        padding: 10
+    }
+})
